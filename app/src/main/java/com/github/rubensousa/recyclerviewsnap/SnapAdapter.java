d133f0dc161cad7c308baf6dbb9c596066942091
ruben <rubensousa.mieti@gmail.com>
2017-03-25 15:53:59 +0000

    Fix vertical mode not opening

--- app/src/main/java/com/github/rubensousa/recyclerviewsnap/SnapAdapter.java
+++ app/src/main/java/com/github/rubensousa/recyclerviewsnap/SnapAdapter.java
@@ -85,7 +85,8 @@ public class SnapAdapter extends RecyclerView.Adapter<SnapAdapter.ViewHolder> im
                     .recyclerView.getContext(), LinearLayoutManager.HORIZONTAL, false));
             holder.recyclerView.setOnFlingListener(null);
             new GravitySnapHelper(snap.getGravity(), false, this).attachToRecyclerView(holder.recyclerView);
-        } else if (snap.getGravity() == Gravity.CENTER_HORIZONTAL) {
+        } else if (snap.getGravity() == Gravity.CENTER_HORIZONTAL ||
+                snap.getGravity() == Gravity.CENTER_VERTICAL) {
             holder.recyclerView.setLayoutManager(new LinearLayoutManager(holder
                     .recyclerView.getContext(), snap.getGravity() == Gravity.CENTER_HORIZONTAL ?
                     LinearLayoutManager.HORIZONTAL : LinearLayoutManager.VERTICAL, false));

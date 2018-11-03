ca3ed37fddbd2dae9e450c6c5670a8803d6e2119
ruben <rubensousa.mieti@gmail.com>
2018-11-03 21:58:49 +0000

    Migrate to androidX

--- gravitysnaphelper/src/main/java/com/github/rubensousa/gravitysnaphelper/GravityPagerSnapHelper.java
+++ gravitysnaphelper/src/main/java/com/github/rubensousa/gravitysnaphelper/GravityPagerSnapHelper.java
@@ -1,17 +1,19 @@
 package com.github.rubensousa.gravitysnaphelper;
 
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.PagerSnapHelper;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class GravityPagerSnapHelper extends PagerSnapHelper {
 
-    @NonNull private final GravityDelegate delegate;
+    @NonNull
+    private final GravityDelegate delegate;
 
     public GravityPagerSnapHelper(int gravity) {
         this(gravity, false, null);
--- gravitysnaphelper/src/main/java/com/github/rubensousa/gravitysnaphelper/GravitySnapHelper.java
+++ gravitysnaphelper/src/main/java/com/github/rubensousa/gravitysnaphelper/GravitySnapHelper.java
@@ -17,10 +17,10 @@
 
 package com.github.rubensousa.gravitysnaphelper;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearSnapHelper;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 
 
